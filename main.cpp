49f95dbac093a0a688799f317db74fa84be3de7f
王京 <s20171105119@1.local>
2018-06-21 09:36:42 +0800

    hello word

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  text.621
+//
+//  Created by  王京 on 18/6/21.
+//  Copyright © 2018年  王京. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!\n";
+    return 0;
+}

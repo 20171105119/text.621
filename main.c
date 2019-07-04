93fd3ba39523fa02cafa48dc577e1c9f95cc8a31
20171105119 <40412880+20171105119@users.noreply.github.com>
2019-07-04 22:10:48 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,108 +1,34 @@
+//
+//  main.c
+//  wj1
+//
+//  Created by 王京 on 2017/10/29.
+//  Copyright © 2017年 王京. All rights reserved.
+//
+
 #include"stdio.h"
-#include"string.h"
 int main()
 {
-    FILE *a;
-    int i=0,j=0;
-    char b[50][50][100];
-    int c[50][50];
-    int max=0;
-    int min=100;
-    int average=0;
-    int lastscore=0;
-    int allscore=0;
-    int n=0;
-    FILE *p;
-    FILE *q;
-    if((a=fopen("/Users/wangjing/Desktop/student1.csv","r"))==0)
+    int i,j,t,n,a[10][10],flag=1;
+    scanf("%d\n",&t);
+    for(i=1;i<t;i++)
     {
-        printf("文件不存在\n");
-    }
-    else
-    {
-        char buf[1000];
-        p=fopen("/Users/wangjing/Desktop/student1.csv","r");
-        
-        while(fgets(buf,1000,p))
-        {
-            n++;
-        }
-        fclose(p);
-        
+        scanf("%d\n",&n);
         for(i=0;i<n;i++)
-        {
-            for(j=0;j<11;j++)
+            for(j=0;j<n;j++)
             {
-                
-                fscanf(a,"%[^,],",b[i][j]);
-                printf("%s ",b[i][j]);
-                
-                //printf("%d ",i);
-                //printf("%d ",j);
-                
-            }
-        }
-        printf("\n");
-        //  printf("%s",b[0][1]);
-        for(i=1;i<n;i++)
-        {
-            for(j=6;j<11;j++)
-            {
-                c[i][j]=(b[i][j]);
-            }
-        }
-        for(i=1;i<n;i++)
-        {
-            for(j=6;j<11;j++)
-            {
-                if(c[i][j]>=max)
-                    max=c[i][j];
-                if(c[i][j]<=min)
-                    min=c[i][j];
-                allscore+=c[i][j];
-            }
-            j=0;
-            lastscore=allscore-min-max;
-            average=lastscore/3;
-            printf("%s  ",b[i][j+1]);
-            printf("average=%d\n",average);
-            lastscore=0;
-            average=0;
-            allscore=0;
-            max=0;
-            min=100;
-        }
-    }
-    q=fopen("/users/wangjing/Desktop/student2.csv","w");
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<11;j++)
-        {
-            
-            fprintf(q,"%s   ",b[i][j]);
-        }
-    }
-    fprintf(q,"\n");
-    for(i=1;i<n;i++)
-    {
-        for(j=6;j<11;j++)
-        {
-            if(c[i][j]>=max)
-                max=c[i][j];
-            if(c[i][j]<=min)
-                min=c[i][j];
-            allscore+=c[i][j];
-        }
-        j=0;
-        lastscore=allscore-min-max;
-        average=lastscore/3;
-        fprintf(q,"%s  ",b[i][j+1]);
-        fprintf(q,"average=%d\n",average);
-        lastscore=0;
-        average=0;
-        allscore=0;
-        max=0;
-        min=100;
-    }
+                scanf("%d",&a[i][j]);
+                for(i=0;i<n;i++)
+                    for(j=0;i<j;j++)
+                        if(a[i][j]!=0)
+                        {
+                            flag=0;
+                            break;}
+                if(flag==1)
+                    printf("YES\n");
+                else printf("NO\n");
+            }}
+    
+    return 0;
 }
 
